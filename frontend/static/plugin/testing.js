de44182d477c57e801321bec365696b04dc05f6a
Tyler Moore <tylerrmoore@gmail.com>
2024-07-06 17:28:50 -0700

    Working synchronous

--- frontend/static/plugin/testing.js
+++ frontend/static/plugin/testing.js
@@ -1,3 +1,4 @@
+
 function onload (event)
 {
     alert("Result loaded.");
@@ -18,13 +19,17 @@ function waiting ()
 
 function TestMessage()
 {
+    var async_request=false;
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:5173/test_xml", true);
-    xhr.setRequestHeader('Content-Type', 'text/plain');
+    xhr.open("POST", "http://localhost:5173/test_xml", async_request);
+    xhr.setRequestHeader('Content-Type', 'application/json'); //Leave this content type regardless of what's actually contained. 'plain/text' caused CSRF protection to manifest.
     var message="This is a test";
     xhr.send(message);
+
+    //alert("Response: " + xhr.readyState + " " + xhr.status + " " + xhr.statusText + " " + xhr.responseText);
     
 
+    /*
     while(xhr.readyState !=4)
     {
         setTimeout(waiting, 500);
@@ -37,5 +42,5 @@ function TestMessage()
     {
         alert("Wrong xhr status:" + xhr.statusText);
     }
-
+    */
 }
