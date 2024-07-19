79bad38feaf6b7a889c9bc626318c1cb7db3eedb
Tyler Moore <tylerrmoore@gmail.com>
2024-07-19 14:59:48 -0700

    Tauri app

--- shim/src/ts/testing.ts
+++ shim/src/ts/testing.ts
@@ -1,12 +1,6 @@
 function RunTest() {
     var result = QueryMRN(2070192);
     Shim_Debug(JSON.stringify(result));
-
-    Shim_Debug("href=" + window.location.href);
-    Shim_Debug("pathname=" + window.location.pathname);
-    Shim_Debug("search=" + window.location.search);
-    Shim_Debug("hash=" + window.location.hash);
-
     //StartHeartbeat();
 }
 
