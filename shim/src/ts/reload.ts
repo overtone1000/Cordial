7401eed1c96eb363bc869f9d1f0dcb2c5222dfab
Tyler Moore <tylerrmoore@gmail.com>
2024-07-19 10:38:58 -0700

    Reload works with local fs

--- shim/src/ts/reload.ts
+++ shim/src/ts/reload.ts
@@ -1,12 +1,10 @@
 function Reload() {
     var newpathname=window.location.pathname;
     var file_prefix="file://";
-    if(window.location.href.length>file_prefix.length && window.location.href.substring(0,file_prefix.length+1)===file_prefix)
+
+    if(window.location.href.length>file_prefix.length && window.location.href.substring(0,file_prefix.length)===file_prefix)
     {
         newpathname=file_prefix+newpathname;
     }
     window.location.href=newpathname;
-
-    //window.location.reload()
-    return false;
 }
