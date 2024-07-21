2cb017aa3bb5eac0a18891e67c5e6ba25e4c83d9
Tyler Moore <TylerRMoore@gmail.com>
2024-07-21 14:54:04 -0700

    More API work

--- application/src-tauri/src/lib.rs
+++ application/src-tauri/src/lib.rs
@@ -1,15 +1,11 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
+use server::shim_post::ShimCommand;
 use warp::Filter;
 
 const SHIM_ADDRESS: u16 = 43528;
 
 pub mod server;
-
-#[derive(Clone, Debug)]
-struct Command {
-    command: String,
-}
 
 pub async fn tokio_serve() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
@@ -20,12 +16,13 @@ pub async fn tokio_serve() {
         format!("Hello, {}!", name)
     };
 
-    let command: Command;
+    let command: ShimCommand = ShimCommand::empty();
+
     let shim_route = warp::post()
         .and(warp::path("shim"))
         .and(warp::path::end())
         .and(warp::body::json())
-        .and(warp::any().map(move || command.clone()))
+        .and(server::shim_post::map_command(command.clone()))
         .and_then(server::shim_post::handle);
 
     let hello_route = warp::path!("hello" / String).map(hello_handler);
--- application/src-tauri/src/server/shim_post.rs
+++ application/src-tauri/src/server/shim_post.rs
@@ -1,6 +1,36 @@
+use serde::{Deserialize, Serialize};
+use std::convert::Infallible;
+use warp::Filter;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub(crate) enum CommandType {
+    Debug,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub(crate) struct ShimCommand {
+    command: CommandType,
+}
+
+impl ShimCommand {
+    pub fn empty() -> ShimCommand {
+        ShimCommand {
+            command: CommandType::Debug,
+        }
+    }
+}
+
+pub(crate) fn map_command(
+    command: ShimCommand,
+) -> impl Filter<Extract = (ShimCommand,), Error = Infallible> + Clone {
+    warp::any().map(move || command.clone())
+}
+
 pub(crate) async fn handle(
-    command: crate::Command,
-    other: String,
+    mut something: String,
+    command: ShimCommand,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     let reply: String = format!("Received: {:?}", command);
     Ok(warp::reply::json(&reply))
