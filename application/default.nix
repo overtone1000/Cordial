dacd567b6c73751d829be225a321d85d577dfd23
Tyler Moore <TylerRMoore@gmail.com>
2024-08-03 15:52:26 -0700

    Trying to create nix build env

---
+++ application/default.nix
@@ -0,0 +1,16 @@
+# default.nix
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+    name = "dev-environment"; # Probably put a more meaningful name here
+    buildInputs = [ 
+        pkg-config 
+        dbus 
+        openssl 
+        glib 
+        gtk3 
+        libsoup 
+        #webkitgtk
+        webkitgtk-2.38.3
+        appimagekit 
+    ];
+}
