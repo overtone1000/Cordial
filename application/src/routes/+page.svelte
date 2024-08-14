731e65f0251aa99f355f32be54883e1828a09c3e
Tyler Moore <TylerRMoore@gmail.com>
2024-08-13 17:39:42 -0700

    Styling

--- application/src/routes/+page.svelte
+++ application/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Main from '../components/main.svelte';
+</script>
+
+<svelte:head>
+	<title>Cordial</title>
+</svelte:head>
+
+<Main />
