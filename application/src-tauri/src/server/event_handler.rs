a5275de890ce3e2f6f1de5b55b856ea93b24bca4
Tyler Moore <TylerRMoore@gmail.com>
2024-09-17 19:53:28 -0700

    Migrate to external library for hyper commons

--- application/src-tauri/src/server/event_handler.rs
+++ application/src-tauri/src/server/event_handler.rs
@@ -1,20 +1,22 @@
+use crate::shim_api::events::ShimEvent;
 use http_body_util::{BodyExt, Full};
 use hyper::{
     body::{Bytes, Incoming},
     service::Service,
     Request, Response,
 };
-
-use crate::shim_api::events::ShimEvent;
+use hyper_trm::generic_service::{
+    full_to_boxed_body, HandlerError, HandlerFuture, HandlerResponse, HandlerResult,
+};
 use std::{future::Future, pin::Pin};
 
 #[derive(Clone)]
 pub struct EventHandler {}
 
 impl Service<Request<Incoming>> for EventHandler {
-    type Response = Response<Full<Bytes>>;
-    type Error = Box<dyn std::error::Error + Send + Sync>;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    type Response = HandlerResponse;
+    type Error = HandlerError;
+    type Future = HandlerFuture;
 
     fn call(&self, request: Request<Incoming>) -> Self::Future {
         let result = Self::handle_poll(self.clone(), request);
@@ -27,10 +29,7 @@ impl EventHandler {
         EventHandler {}
     }
 
-    async fn handle_poll(
-        self: EventHandler,
-        request: Request<Incoming>,
-    ) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>> {
+    async fn handle_poll(self: EventHandler, request: Request<Incoming>) -> HandlerResult {
         let method = request.method().clone();
         let path = request.uri().path().to_string();
         let headers = request.headers().clone();
@@ -51,11 +50,11 @@ impl EventHandler {
                     method, path, headers, as_string
                 );
                 eprintln!("Error getting event from string. {:?}", &as_string);
-                return Ok(Response::new(Full::new(Bytes::from("Invalid JSON"))));
+                return Ok(Response::new(full_to_boxed_body("Invalid JSON")));
             }
         };
 
-        return Ok(Response::new(Full::new(Bytes::from("Ok"))));
+        return Ok(Response::new(full_to_boxed_body("Ok")));
     }
 
     fn process_event(&self, event: ShimEvent) -> Result<(), String> {
@@ -63,15 +62,15 @@ impl EventHandler {
             ShimEvent::Debug(message) => println!("Debug message from shim: {}", message),
             ShimEvent::PageStatus(canvas_page_id, shelf_id, visible) => {
                 println!("Shelf loaded:{} {} {}", canvas_page_id, shelf_id, visible)
-            },
+            }
             ShimEvent::QueryResult(result) => {
                 println!("Query result::{:?}", result);
-            },
+            }
             ShimEvent::Logout => {
                 println!("Logout.");
-            },
+            }
             _ => {
-                eprintln!("Unhandled event: {:?}",event);
+                eprintln!("Unhandled event: {:?}", event);
             }
         };
 
