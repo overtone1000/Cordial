5ac2c265febf26c3c93ede17cb9a250002ca8b48
Tyler Moore <tylerrmoore@gmail.com>
2024-09-18 21:43:53 -0700

    Only post if enabled

--- shim/src/ts/components/events/post.ts
+++ shim/src/ts/components/events/post.ts
@@ -2,55 +2,48 @@
 type ShimPost = ShimEvent | ShimCallResponse;
 
 var event_url = "http://localhost:43528/";
-var TIMEOUT=1000;
+var TIMEOUT = 1000;
 
 var xhr_event = new XMLHttpRequest(); //Use one instance to avoid memory leak
 
-function eventAbortFunction()
-{
-    try
-    {
-        if (xhr_event.readyState !== XMLHttpRequest_DONE)
-        {
+function eventAbortFunction() {
+    try {
+        if (xhr_event.readyState !== XMLHttpRequest_DONE) {
             xhr_event.abort();
         }
     }
-    catch(e)
-    {
+    catch (e) {
         console.info(e);
     }
 }
 
 function Send_Event_Post(post: ShimPost) {
-    try
-    {
-        xhr_event.open("POST", event_url, true);   
+    try {
+        xhr_event.open("POST", event_url, true);
         xhr_event.timeout = TIMEOUT;
         xhr_event.setRequestHeader('Content-Type', 'application/json'); //Leave this content type as application. 'plain/text' caused CSRF protection to manifest.
         xhr_event.send(JSON.stringify(post));
 
-        setTimeout(eventAbortFunction,TIMEOUT*2); //Trying this to address black screen
+        setTimeout(eventAbortFunction, TIMEOUT * 2); //Trying this to address black screen
     }
-    catch(e)
-    {
+    catch (e) {
         console.info(e);
     }
 }
 
 function Send_Event(post: ShimPost) {
     //setTimeout(Send_Event_Post,0,[post]); //Argument not passed when using iSite?
-    //Send_Event_Post(post); //freezes UI to call directly even if asynchronous    
-    try
-    {
-        var f = function ()
-        {
-            Send_Event_Post(post);
-        }
+    //Send_Event_Post(post); //freezes UI to call directly even if asynchronous
+    if (enabled) {
+        try {
+            var f = function () {
+                Send_Event_Post(post);
+            }
 
-        setTimeout(f,0);
-    }
-    catch(e)
-    {
-        console.info(e);
+            setTimeout(f, 0);
+        }
+        catch (e) {
+            console.info(e);
+        }
     }
 }
